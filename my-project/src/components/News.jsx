4b65bb89d8633c4c5bb09860ced78dadde1050fd
Piotr Brozek <1brozek.peter@gmail.com>
2024-07-30 20:32:05 -0500

    able to filter through irrelevant articles

--- my-project/src/components/News.jsx
+++ my-project/src/components/News.jsx
@@ -7,43 +7,47 @@ const News = () => {
 
   const [news, setNews] = useState();
 
-
-
   useEffect(() => {
-    const fetchNews = () => {
-      const apiServer = `http://localhost:3500`;
-      const query = "AAPL";
-  
-      fetch(`${apiServer}/getNews?inquiry=${query}`)
-        .then((res) => res.json())
-        .then((data) => {
+    const fetchNews = async () => {
+      if (ticker.length !== 0) {
+        const apiServer = `http://localhost:3500`;
+        const query = ticker[ticker.length - 1].symbol;
+
+        try {
+          const response = await fetch(`${apiServer}/getNews?inquiry=${query}`);
+          const data = await response.json();
           console.log("data is ", data);
-          //filtering through all articles for search term
+
+          // Filtering articles for the search term
           const filteredArticles = data.articles.filter(
             (article) =>
               article.title.toLowerCase().includes(query.toLowerCase()) ||
               article.description.toLowerCase().includes(query.toLowerCase())
           );
-  
+
           console.log(`${apiServer}/getNews?inquiry=${query}`);
-  
-          setNews(filteredArticles);
-          console.log("filtered news is ", filteredArticles);
-        })
-        .catch((error) => {
+
+          if (filteredArticles.length > 0) {
+            setNews(filteredArticles);
+            console.log("filtered news is ", filteredArticles);
+          } else {
+            console.log("No articles found for the query, getting broader news..");
+            setNews(data);
+            console.log("Broader news is ", data.articles);
+          }
+        } catch (error) {
           console.error("Error fetching news:", error);
-        });
+        }
+      }
     };
-  
+
     fetchNews();
   }, [ticker]);
-  
 
 
   return (
     <Card>
       <div>News</div>
-   
     </Card>
   );
 };
